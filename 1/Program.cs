783e334d6a5059f741ea5dd1ec246ccba010c79f
3v <jarovcev@mail.ru>
2022-09-04 11:59:06 +0300

    first commit

---
+++ 1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа которая сравнивает 2 числа.
+Console.Write("Введите первое число:  ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write( "Введите второе число:  " );
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+    Console.Write (  a );
+if (b > a)
+    Console.Write (  b ); 
+if (b == a)
+    Console.Write ( "ERROR");
