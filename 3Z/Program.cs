011dee6f44484f91c7861bfa4e3cf6e5a02e33df
3v <jarovcev@mail.ru>
2022-09-04 13:41:25 +0300

    3задание готово

---
+++ 3Z/Program.cs
@@ -0,0 +1,7 @@
+﻿// Программа которая опр. четность или не четность числа.
+Console.Write("Введите число для проверка четности:  ");
+int a = Convert.ToInt32(Console.ReadLine());
+if ( a % 2 == 0 )
+    Console.WriteLine ( "Четное");
+else
+    Console.WriteLine ( "Не четное");
