b4d5b6e704a2dca57251dfea03ae3d3c13232615
3v <jarovcev@mail.ru>
2022-09-04 16:23:14 +0300

    Все ДЗ готово

--- 4task/Program.cs
+++ 4task/Program.cs
@@ -2,5 +2,10 @@
 Console.Write("Введите число:  ");
 int n = Convert.ToInt32(Console.ReadLine());
 for (int i = 0; i <=n; i+=2 )
-    Console.Write(i + "  ");
+{
+    if ( i == n || i == n - 1)
+        Console.Write(i);
+    else
+        Console.Write(i + ", ");
+}
 // Сам я не особо допер, пытался все сделать через IF, по этому уже переписал на разборе ДЗ.
